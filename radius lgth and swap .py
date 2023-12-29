26b85c90c770724967b2852ef7402e5a9c91a1f4
parikadarsh573 <154779900+parikadarsh573@users.noreply.github.com>
2023-12-29 12:10:02 +0530

    Add files via upload

---
+++ radius lgth and swap .py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+l=int(input("enter number"))
+b=int(input("enter number"))
+area=l*b
+print(area)
+
+
+# In[5]:
+
+
+pi=31.4
+r=float(input("enter number"))
+area=pi*r**r
+print(area)
+
+
+# In[11]:
+
+
+celsions=float(input("enter temperature"))
+ferenhit=(celsions*9/5)+32
+print(celsions)
+print(ferenhit)
+
+
+# In[12]:
+
+
+p=int(input("Enter principal amount"))
+t=int(input("Enter time amount"))
+r=int(input("Enter rate amount"))
+si=(p*t*r)/100
+print(si)
+
+
+# In[15]:
+
+
+a=2
+b=3
+c=4
+result=(a+b+c)/2
+print(result)
+
+
+# In[16]:
+
+
+a=5
+b=6
+d=4
+e=2
+c=(a+b)+(d*e)
+print(c)
+
+
+# In[17]:
+
+
+a=5
+b=6
+print(a,b)
+temp=a
+a=b
+b=temp
+print(a,b)
+
+
+# In[ ]:
+
+
+
+
